4b572219b7e7852e6fe1411407dc2fcf08b1c2d0
Takeshi Ishita <ishitah.takeshi@gmail.com>
2014-07-26 11:45:38 +0900

    Add tree.py

---
+++ forest/tree.py
@@ -0,0 +1,17 @@
+import libtree
+
+class DecisionTree(object):
+    def __init__(self, leaf_size=1):
+        self.leaf_size = leaf_size
+        self.tree = libtree.DecisionTree()
+        self.fit_finished = False
+
+    def fit(self, X, y):
+        assert len(X) == len(y), "X and y must be the same size."
+        self.tree.fit(X, y, self.leaf_size)
+        self.fit_finished = True
+        return self
+
+    def predict(self, X):
+        assert self.fit_finished, "Train before predicting."
+        return self.tree.predict(X)
