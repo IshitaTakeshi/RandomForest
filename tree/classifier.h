606d41104fcb421e7e11b2e6e0264156c8aa5102
Takeshi Ishita <ishitah.takeshi@gmail.com>
2014-07-26 11:14:50 +0900

    Rename the argument of free_tree

--- tree/classifier.h
+++ tree/classifier.h
@@ -4,4 +4,4 @@
 Node *fit(double **vectors, int *labels, int n_vectors, int n_dim, int leaf_size);
 int *predict(Node *tree, double **vectors, int n_vectors);
 double measure_accuracy(Node *tree, double **vectors, int *labels, int n_vectors);
-void free_tree(Node *node);
+void free_tree(Node *tree);
