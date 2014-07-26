88cabbafbfd5f5bc5c5a85045458db8ba799fc3e
Takeshi Ishita <ishitah.takeshi@gmail.com>
2014-07-26 11:53:11 +0900

    Add tree_test.py

---
+++ forest/tree_test.py
@@ -0,0 +1,15 @@
+from tree import DecisionTree
+from iris_dataset import vectors, labels
+
+N = int(len(vectors)*0.8)
+training_vectors = vectors[:N]
+training_labels = labels[:N]
+test_vectors = vectors[N:]
+test_labels = labels[N:]
+
+tree = DecisionTree(leaf_size=1)
+tree.fit(training_vectors, training_labels)
+results = tree.predict(test_vectors)
+
+print("results:{}".format(results))
+print("answers:{}".format(test_labels))
